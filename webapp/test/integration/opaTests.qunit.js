ae96e025517dc5751632687cc77f5b9076205cf9
Judith Sanchez <judith.sanchez-marty.external@capgemini.com>
2024-03-19 09:05:24 +0000

    Initial Commit @3/19/2024, 9:05:24 AM

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["student00/com/sap/training/ux402/opa/ux402opa/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
